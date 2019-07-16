93e4bda72e87f1e6fbc0adbb102343cb881d436b
jonahrosenblum <jonaherosenblum@gmail.com>
2019-07-16 04:01:22 +0100

    python2/3 compatability and fixed write buffer bug

--- setup.py
+++ setup.py
@@ -4,17 +4,24 @@ import sys
 if len(sys.argv) != 2:
     print('Usage: sudo python/python3 setup.py basic/sensor')
 
+# python2/3 compatibility
+try: input = raw_input
+except NameError: pass
+
 if sys.argv[1] == 'basic':
     os.system('sudo apt-get --assume-yes install wiringpi')
 
 elif sys.argv[1] == 'sensor_step1':
     with open('/boot/config.txt', 'a') as config:
-        config.write('dtoverlay=w1-gpio,gpio-pin=' +
+        config.write('\ndtoverlay=w1-gpio,gpio-pin=' +
                 str(input('Enter the pin number of your sensor\'s SIG wire:\n')))
-        if str(raw_input('As part of the setup, you must reboot your pi. Enter `y` to reboot:\n')) == 'y':
-            os.system('sudo reboot')
-        else:
-            print('Your step 1 setup is not yet complete until you reboot your pi')
+        config.flush()
+        os.fsync(config)
+
+    if str(input('As part of the setup, you must reboot your pi. Enter `y` to reboot:\n')) == 'y':
+         os.system('sudo reboot')
+    else:
+         print('Your step 1 setup is not yet complete until you reboot your pi')
 
 elif sys.argv[1] == 'sensor_step2':
     os.system('sudo modprobe w1-gpio')

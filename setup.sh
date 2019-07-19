aa7187ca98eb66411552df7963ccd512e6197f74
jonahrosenblum <jonaherosenblum@gmail.com>
2019-07-18 21:52:42 -0400

    added setup bash script

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# setup
+
+# stop on errors
+set -e
+
+# sanity check command line options
+usage() {
+    echo "Usage: $0 (basic|sensor_step1|sensor_step2)"
+}
+
+if [ $# -ne 1 ]; then
+    usage
+    exit 1
+fi
+
+# parse args, $1 is the first argument
+case $1 in
+    "basic")
+        sudo apt-get --assume-yes install wiringpi
+        ;;
+
+    "sensor_step1")
+        read -p "Enter the pin number of your sensor's SIG wire: "  pin
+        sudo echo "dtoverlay=w1-gpio,gpio-pin="$pin | sudo tee -a /boot/config.txt
+        read -p "To continue installing you must restart your pi, enter `y` to reboot. "  response
+        if [ $response = "y" ]; 
+        then
+            reboot;
+        else;
+            echo "You will not be able to run the second step until you reboot."
+        fi
+        ;;
+
+    "sensor_step2")
+        sudo modprobe w1-gpio
+        sudo modprobe w1-therm
+        ;;
+esac
